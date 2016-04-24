f2bc607cee6e0859935916d82398f2b68607176c
Michael Howlett <mah9882@rit.edu>
2016-04-24 12:38:40 -0400

    Created an ExternalReader class to be implemented by the LoadContent method to load images in by filepath from the binary file created by our external tool. Not yet finished; still testing.

---
+++ LoveIsWar/LoveIsWar/ExternalReader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//streams
+using System.IO;
+
+namespace LoveIsWar
+{
+    class ExternalReader
+    {
+        //array containing values
+        string[] filenames = new string[8];
+        //CHANGE LENGTH OF STRING ARRAY TO ADJUST FOR ADDITIONAL FILES
+
+
+        //base constructor
+        public ExternalReader()
+        {
+
+        }
+
+        //method to read in a binary file
+        public void Read(string filename)
+        {
+            //try to catch exceptions
+            try
+            {
+               
+                //open a stream
+                Stream str = File.OpenRead(filename);
+
+                //make a streamreader reading from the file
+                BinaryReader input = new BinaryReader(str);
+
+                //read from the stream in a loop and set each value to the array
+                for(int i = 0; i < filenames.Length; i++)
+                {
+                    string line = input.ReadString();
+                    filenames[i] = line;
+                }
+
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Test()
+        {
+            //try/catch
+            try
+            {
+                for(int i = 0; i < filenames.Length; i++)
+                {
+                    Console.WriteLine(filenames[i]);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
