e30c17968d02ce30737075f55c6866465df2b5fe
William Pennock (RIT Student) <wxp9803@ad.rit.edu>
2016-04-27 10:47:33 -0400

    Added a high score class. Frame work for the high score class is there with a read score method.

---
+++ LoveIsWar/LoveIsWar/Highscore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LoveIsWar
+{
+    class Highscore
+    {
+        public string strScore;
+        public int score;
+        public int readScore()
+        {
+            StreamReader input = new StreamReader("Highscore");
+
+            strScore = input.ReadLine();
+            int.TryParse(strScore, out score);
+            //closes the file
+            input.Close();
+            return score;
+        }
+
+
+
+
+
+    }
+}
